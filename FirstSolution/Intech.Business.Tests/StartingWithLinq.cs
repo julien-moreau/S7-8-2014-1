4dd616622898d27337fc4d70071e5b5ca43ce835
Olivier Spinelli <olivier.spinelli@invenietis.com>
2014-04-04 17:33:54 +0200

    MiniLinq

--- FirstSolution/Intech.Business.Tests/StartingWithLinq.cs
+++ FirstSolution/Intech.Business.Tests/StartingWithLinq.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -31,13 +30,42 @@ namespace Intech.Business.Tests
         {
             CollectionAssert.AreEqual( Numbers( 0, 5 ), new[] { 0, 1, 2, 3, 4 } );
 
-            int notTooMuch = 5454;
+            int notTooMuch = 10;
             foreach( var i in Sawtooth( 12 ) )
             {
                 Console.WriteLine( i );
                 if( --notTooMuch == 0 ) break;
             }
 
+            var noLimit = Sawtooth( 3 );
+            var limited = MiniLinqExtension.Limit( noLimit, 9 );
+            CollectionAssert.AreEqual( limited, new[] { 0, 1, 2, 0, 1, 2, 0, 1, 2 } );
+
+            var onlyThe5Last = MiniLinqExtension.Skip( limited, 4 );
+            CollectionAssert.AreEqual( onlyThe5Last, new[] { 1, 2, 0, 1, 2 } );
+
+            var source = RandomNumbers( 154 );
+            var the13ThatInterestMe = MiniLinqExtension.Limit( MiniLinqExtension.Skip( source, 20 ), 13 );
+
+            var simpler = source.Skip( 20 ).Limit( 13 );
+        }
+
+        [Test]
+        public void MiniLinqInAction()
+        {
+            var evenRandomNumbers = RandomNumbers( 125 ).Where( x => x % 2 == 0 );
+            var simpler = RandomNumbers( 125 ).Select( x => x * 2.0 );
+            var toString = RandomNumbers( 125 ).Select( x => x.ToString() );
+
+            var complexSerie = RandomNumbers(12).Select( x => new { R = x, I = x * 2 } );
+
+            var basedOnComplex = complexSerie
+                                    .Where( c => c.I > 0 )
+                                    .Limit( 50 );
+
+            var linq = from x in RandomNumbers( 125 )
+                       where x > 7
+                       select x;
 
         }
 
